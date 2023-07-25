d0dedf9ad6280ccc1b14df293982326a443c972c
Manuel Hrgetic <128990687+mhrgetic5@users.noreply.github.com>
2023-07-25 18:02:17 +0200

    Add files via upload

---
+++ Controller/ItemsController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ItemsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ItemsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/Items
+    [HttpGet]
+    public async Task<IActionResult> GetItems()
+    {
+        var items = await _context.Items.ToListAsync();
+        return Ok(items);
+    }
+
+    // GET: api/Items/5
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetItem(int id)
+    {
+        var item = await _context.Items.FindAsync(id);
+
+        if (item == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(item);
+    }
+
+    // POST: api/Items
+    [HttpPost]
+    public async Task<IActionResult> CreateItem(Item item)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest();
+        }
+
+        _context.Items.Add(item);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item);
+    }
+
+    // PUT: api/Items/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateItem(int id, Item item)
+    {
+        if (id != item.Id || !ModelState.IsValid)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(item).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    // DELETE: api/Items/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteItem(int id)
+    {
+        var item = await _context.Items.FindAsync(id);
+
+        if (item == null)
+        {
+            return NotFound();
+        }
+
+        _context.Items.Remove(item);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
